98c2ce01fd3ca60e9d9d6c9c2a021f4e88abffc7
Kipras Melnikovas <kipras@kipras.org>
2021-10-15 01:34:45 +0300

    feat: init npm pkg to add types(cript)
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

---
+++ types.d.ts
@@ -0,0 +1,37 @@
+// @ts-check
+
+/**
+ *
+ * https://roamresearch.com/#/app/help/page/Nxz8u0vXU
+ *
+ */
+
+export type EntityBase = {
+	"create-time": number;
+	"edit-time": number;
+	"edit-email": string;
+};
+
+export type Heading = 0 | 1 | 2 | 3;
+export type TextAlign = "left" | "center" | "right" | "justify";
+
+export type Block = EntityBase & {
+	string: string;
+	uid: string;
+	children: Block[];
+	heading: Heading;
+	"text-align": TextAlign;
+};
+
+export type Page = EntityBase & {
+	title: string;
+	children: Block[];
+};
+
+export type PageWithMetadata = {
+	page: Page | Block;
+	hasPublicTag: boolean;
+	isPublicTagInRootBlocks: boolean;
+};
+
+export type FindPublicPages = (pagesOrTheirChildren: (Page | Block)[], isRoot?: boolean) => PageWithMetadata[];
