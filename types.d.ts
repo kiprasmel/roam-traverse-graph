2d3c7b75d6e8384ae625d874f5662e29846c385c
Kipras Melnikovas <kipras@kipras.org>
2021-10-15 06:14:29 +0300

    TEMP
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- types.d.ts
+++ types.d.ts
@@ -1,32 +1,6 @@
-// @ts-check
+import { Page, Block } from "./roam";
 
-/**
- *
- * https://roamresearch.com/#/app/help/page/Nxz8u0vXU
- *
- */
-
-export type EntityBase = {
-	"create-time": number;
-	"edit-time": number;
-	"edit-email": string;
-};
-
-export type Heading = 0 | 1 | 2 | 3;
-export type TextAlign = "left" | "center" | "right" | "justify";
-
-export type Block = EntityBase & {
-	string: string;
-	uid: string;
-	children: Block[];
-	heading: Heading;
-	"text-align": TextAlign;
-};
-
-export type Page = EntityBase & {
-	title: string;
-	children: Block[];
-};
+export * from "./roam";
 
 export type FindPublicPagesOptions = {
 	publicTag?: string;
