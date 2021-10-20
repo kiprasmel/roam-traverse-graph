ab997e002bff525b1a2c7eac151f3666da4c527e
Kipras Melnikovas <kipras@kipras.org>
2021-10-20 04:59:10 +0300

    pool promises to avoid reaching rate limits
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- util.js
+++ util.js
@@ -12,7 +12,30 @@ const getAllBlocksFromPages = (somePages = []) =>
 	(!somePages || !somePages.length) ? [] : somePages.map((p) => [p.uid, ...getAllBlocksFromPages(p.children)]).flat();
 //, ...(!p.children ? [] : p.children.map((c) => getAllBlocksFromPages(c)))]).flat();
 
+const delay = (timeoutMs = 0) => new Promise((r) => setTimeout(r, timeoutMs));
+
+const poolPromises = async (intervalMs, maxRequestsPerIntervalIncl, arrayOfPromiseCreators = []) => {
+	const results = [];
+
+	while (arrayOfPromiseCreators.length > 0) {
+		const current = arrayOfPromiseCreators.splice(0, maxRequestsPerIntervalIncl + 1);
+
+		const startTime = new Date().getTime();
+		const tmpResults = await Promise.all(current.map((promiseCreator) => promiseCreator()));
+		results.push(tmpResults);
+
+		const endTime = new Date().getTime();
+		const delta = endTime - startTime;
+		await delay(intervalMs - delta);
+	}
+
+	return results.flat();
+};
+
+//
 module.exports = {
 	readJsonSync, //
 	getAllBlocksFromPages,
+	delay,
+	poolPromises,
 };
