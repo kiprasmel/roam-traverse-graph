711f2e06e68419f4df89b68a61ca47347b8e92ba
Kipras Melnikovas <kipras@kipras.org>
2021-10-20 03:50:03 +0300

    delete pages AND blocks
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- util.js
+++ util.js
@@ -1,8 +1,18 @@
+// @ts-check
+
 const fs = require("fs");
 const path = require("path");
 
 const readJsonSync = (pathToFile) => JSON.parse(fs.readFileSync(path.resolve(pathToFile), { encoding: "utf-8" }));
 
+/**
+ * @type { (somePages?: import("./types").PageOrBlock[]) => import("./types").EntityBase["uid"][] }
+ */
+const getAllBlocksFromPages = (somePages = []) =>
+	(!somePages || !somePages.length) ? [] : somePages.map((p) => [p.uid, ...getAllBlocksFromPages(p.children)]).flat();
+//, ...(!p.children ? [] : p.children.map((c) => getAllBlocksFromPages(c)))]).flat();
+
 module.exports = {
 	readJsonSync, //
+	getAllBlocksFromPages,
 };
