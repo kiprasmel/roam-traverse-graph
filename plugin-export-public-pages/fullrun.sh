375d4f40268a093559a0eba7f6c43ff812a5eff1
Kipras Melnikovas <kipras@kipras.org>
2021-12-01 13:05:17 +0200

    feat: first check if any untracked changes exist in fullrun.sh
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- plugin-export-public-pages/fullrun.sh
+++ plugin-export-public-pages/fullrun.sh
@@ -6,11 +6,23 @@ PRIVATE_NOTES_DIR="${1:-$HOME/projects/notes-private}"
 PRIVATE_NOTES_USERNAME="${2:-sarpik}"
 PRIVATE_NOTES_REPO_NAME="${3:-notes}" # TODO notes-private by default
 PUBLIC_NOTES_ROOT_URL="${4:-http://kiprasmel.github.io/notes}"
- 
-pushd "$PRIVATE_NOTES_DIR"
+PUBLIC_NOTES_DIR="${5:-notes}"
 
-# create one yourself, until maybe we do a generalised one, hehe.
-bash ./run.sh
+###
+
+pushd "$PUBLIC_NOTES_DIR"
+
+# https://stackoverflow.com/a/9393642/9285308
+[ -z "$(git status -s)" ] || {
+	printf "\nuncommitted changes in PUBLIC_NOTES_DIR ($PUBLIC_NOTES_DIR) ($(pwd))\n\n"
+	exit 1
+}
+
+git pull
+
+popd
+
+###
 
 diffy() {
 	git diff --staged --stat --color=always | less
@@ -21,7 +33,14 @@ commit_push() {
 	git commit --no-gpg-sign -m "$MSG"
 	git push
 }
+
+###
  
+pushd "$PRIVATE_NOTES_DIR"
+
+# create one yourself, until maybe we do a generalised one, hehe.
+bash ./run.sh
+
 git add .
 diffy
 
@@ -31,13 +50,11 @@ COMMIT_SHA="$(git show --pretty=format:'%H' | head -n 1)"
  
 popd
 
-
-
-./generate-static-html-pages.ts
+###
  
+./generate-static-html-pages.ts
 
-
-pushd ./notes
+pushd "$PUBLIC_NOTES_DIR"
 
 git add .
 diffy
@@ -46,6 +63,6 @@ commit_push "deploy (manual): http://github.com/$PRIVATE_NOTES_USERNAME/$PRIVATE
 
 popd
  
-
+###
 
 printf "\n$PUBLIC_NOTES_ROOT_URL\n\n"
