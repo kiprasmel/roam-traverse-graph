7f4f85b322283f5a8c0bd1c2a0681ec993c3294b
Kipras Melnikovas <kipras@kipras.org>
2022-01-02 13:23:21 +0200

    getting close
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

---
+++ plugin-export-public-pages/parseASTFromBlockString.ts
@@ -0,0 +1,259 @@
+import { MutatingActionToExecute } from "../traverseBlockRecursively";
+
+import { Tuple } from "../util/tuple";
+import { withMetadata } from "../util/withMetadata";
+
+const boundaries = [
+	{
+		begin: "```",
+		end: "```",
+		type: "code-block",
+		kind: "whole",
+	},
+	{
+		begin: "`",
+		end: "`",
+		type: "code-block",
+		kind: "inline",
+	},
+	//
+	{
+		// TODO "allow un-beginned" or whatever
+		begin: null,
+		end: "::",
+		// type: "attribute",
+		// // TODO - linked reference w/ kind: attribute?
+		type: "linked-reference", // TODO attribute? or nah? probably not.
+		kind: "::",
+	},
+	{
+		begin: "#[[",
+		end: "]]",
+		type: "linked-reference",
+		kind: "#[[]]",
+		// parse: (s: string): boolean => {
+		// 	//
+		// },
+	},
+	{
+		begin: "[[",
+		end: "]]",
+		type: "linked-reference",
+		kind: "[[]]",
+	},
+	/**
+	 * TODO FIXME:
+	 */
+	// {
+	// 	begin: "#",
+	// 	// end: [" ", ".", ":", "'"],
+	// 	end: " ", // TODO FIXME - use above
+	// 	type: "linked-reference",
+	// 	kind: "#",
+	// 	// TODO "allow unfinished"
+	// },
+	//
+	{
+		begin: "{{",
+		end: "}}",
+		type: "command",
+	},
+	//
+] as const;
+
+export type Boundary = typeof boundaries[number];
+
+export type StackItem =
+	| Tuple<"text", string> //
+	| Tuple<"begin" | "end", Boundary>;
+
+export type StackedTreeTextChild = {
+	type: "text";
+	content: string;
+};
+
+export type StackedTreeBoundaryChild = Boundary & {
+	children: StackedTreeChild[];
+};
+
+export type StackedTreeChild = StackedTreeTextChild | StackedTreeBoundaryChild;
+
+export const parseASTFromBlockString: MutatingActionToExecute<
+	{},
+	{
+		stack: StackItem[];
+		stackTree: StackedTreeChild[];
+	}
+> = () => (block) => {
+	/**
+	 * order matters
+	 */
+	let cursor = 0;
+	const originalString: string = block.string;
+
+	// TODO FIXME
+	const stack: any[] = [];
+
+	function parseUntil(from: string | null, until: string | null): void {
+		const advance = (n: number): string => ((cursor += n), originalString.slice(cursor));
+
+		const str: string = advance(0);
+
+		const startsWith = (s: string): boolean => s === str.slice(0, s.length);
+
+		const foundNonText: boolean = boundaries.some((b): boolean => {
+			if (b.begin === null && b.end === null) {
+				throw new Error("begin & end cannot both be null");
+			} else if (b.begin === null) {
+				if (startsWith(b.end)) {
+					/**
+					 * need everything from very beginning up until now
+					 */
+
+					stack.unshift(["begin", b]);
+
+					/**
+					 * text (or other stuff) have already been parsed
+					 */
+
+					stack.push(["end", b]);
+
+					advance(b.end.length);
+
+					return true;
+				}
+			} else if (b.end === null) {
+				/**
+				 * until we have an actual use case
+				 */
+				// @ts-expect-error
+				if (startsWith(b.begin)) {
+					/**
+					 * need everything from now up until the very end
+					 */
+
+					stack.push(["begin", b]);
+					// @ts-expect-error
+					advance(b.begin.length);
+
+					// @ts-expect-error
+					parseUntil(b.begin, null);
+
+					stack.push(["end", b]);
+
+					return true;
+				}
+			} else if (startsWith(b.begin)) {
+				if (b.begin === b.end && until === b.end) {
+					/**
+					 * do NOT advance NOR push here
+					 * because once we return,
+					 * the stuff right below us will activate
+					 * & do the work there.
+					 */
+
+					stack.push(["end", b]);
+					advance(b.end.length);
+
+					return true;
+				}
+
+				stack.push(["begin", b]);
+				advance(b.begin.length);
+
+				parseUntil(b.begin, b.end);
+
+				return true;
+			} else if (startsWith(b.end)) {
+				if (
+					// (from !== b.begin && from !== null) ||
+					// (until !== b.end && until !== null)
+					(from !== b.begin || until !== b.end) &&
+					(from !== null || until !== null)
+					// TODO FIXME
+				) {
+					// stack.push(["MISMATCH", { from, until, b }]);
+					// TODO INDICATE FAILURE IF NONE MATCH (or should we?)
+					return false;
+					// throw new Error(
+					// 	`unmatched! block.uid = "${block.uid}", block.string (original!) = "${block.string}", cursor was at "${cursor}", until = "${until}", b.end = "${b.end}"`
+					// );
+				} else {
+					/**
+					 * matched!
+					 */
+					stack.push(["end", b]);
+					advance(b.end.length);
+
+					return true;
+				}
+			}
+
+			return false;
+		});
+
+		const curr = advance(0);
+
+		if (curr.length) {
+			if (!foundNonText) {
+				const char = curr[0];
+				stack.push(["char", char]);
+				advance(1);
+
+				return parseUntil(from, until);
+			}
+
+			// return parseUntil(null);
+			// return parseUntil(until);
+		}
+	}
+
+	while (cursor < originalString.length) {
+		parseUntil(null, null);
+	}
+
+	const [stackWithTextInsteadOfChars, leftoverText] = stack.reduce<Tuple<StackItem[], string>>(
+		([acc, tempString], [beginEndChar, item]) =>
+			beginEndChar === "char"
+				? [acc, tempString + item] //
+				: (tempString && acc.push(["text", tempString]), acc.push([beginEndChar, item]), [acc, ""]), //
+		[[], ""]
+	);
+	if (leftoverText) {
+		stackWithTextInsteadOfChars.push(["text", leftoverText]);
+	}
+
+	let i = 0;
+
+	const stackTree: StackedTreeChild[] = toTree();
+
+	function toTree(): StackedTreeChild[] {
+		const childrenAtCurrentLevel: StackedTreeChild[] = [];
+
+		for (; i < stackWithTextInsteadOfChars.length; i++) {
+			const [beginEndText, item] = stackWithTextInsteadOfChars[i];
+
+			if (beginEndText === "text") {
+				childrenAtCurrentLevel.push({
+					type: "text",
+					content: item as Exclude<typeof item, Boundary>,
+				});
+			} else if (beginEndText === "begin") {
+				i++;
+				childrenAtCurrentLevel.push({
+					...(item as Exclude<typeof item, string>),
+					children: toTree(),
+				});
+			} else if (beginEndText === "end") {
+				return childrenAtCurrentLevel;
+			}
+		}
+
+		return childrenAtCurrentLevel;
+	}
+
+	return withMetadata({
+		stack: stackWithTextInsteadOfChars,
+		stackTree,
+	})(block);
+};
