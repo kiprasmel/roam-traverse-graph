1b0b9d627189217fe1de38fa14f3a888f468b8d9
Kipras Melnikovas <kipras@kipras.org>
2022-02-03 03:06:51 +0200

    more test cases for AST parsing; disable broken ones for now
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- plugin-export-public-pages/parseASTFromBlockString.spec.ts
+++ plugin-export-public-pages/parseASTFromBlockString.spec.ts
@@ -1,10 +1,86 @@
-import { testcase, input1, expected1 } from "./test-utils";
+import { Triple } from "../util/tuple";
+
+import { AST, testcase /* input1, expected1 */ } from "./test-utils";
 
 // runMany([["should yeet", testcase(input1), expected1]]);
 
 describe("block string AST parser", () => {
-	it("parses nested linked references", () => {
-		const result = testcase(input1);
-		expect(result).toStrictEqual(expected1);
-	});
+	const testcases: Triple<string, string, AST>[] = [
+		["parses basic text", "hello world", [["text", "hello world"]]], //
+		[
+			"parses command {{}}",
+			"oh {{very}} nice",
+			[
+				["text", "oh "],
+				["command", ["text", "very"]],
+				["text", " nice"],
+			],
+		],
+		[
+			"parses linked reference [[]]",
+			"look [[TODO]] done",
+			[
+				["text", "look "],
+				["linked-reference/[[]]", ["text", "TODO"]],
+				["text", " done"],
+			],
+		],
+		[
+			"parses linked reference :: (attribute)",
+			"parent but with spaces:: done haha leggo",
+			[
+				["linked-reference/::", ["text", "parent but with spaces"]],
+				["text", " done haha leggo"],
+			],
+		],
+		[
+			"parses linked reference #[[]]",
+			"look #[[oh-boy]] done",
+			[
+				["text", "look "],
+				["linked-reference/#[[]]", ["text", "oh-boy"]],
+				["text", " done"],
+			],
+		],
+		[
+			"parses linked reference # when does end with it",
+			"look #oh-boy",
+			[
+				["text", "look "],
+				["linked-reference/#", ["text", "oh-boy"]],
+			],
+		],
+		// TODO
+		/*
+		["parses linked reference # when does NOT end with it", "look #oh-boy done", [
+			["text", "look "],
+			["linked-reference/#", ["text", "oh-boy"]],
+			["text", " done"]
+		]],
+		*/
+		// TODO
+		/*
+		[
+			"parses inline code block",
+			"hello inline `code block` haha",
+			[
+				["text", "hello inline "],
+				["code-block/inline", ["text", "code block"]],
+				["text", " haha"]
+			],
+		],
+		//
+		["parses whole code block", "hello whole ```code block hehe\nnice``` mhm", [
+			["text", "hello whole "],
+			["code-block/whole", ["text", "code block hehe\nnice"]],
+			["text", " mhm"]
+		]],
+		*/
+	];
+
+	for (const [should, input, expectedOutput] of testcases) {
+		it(should, () => {
+			expect(testcase(input)).toStrictEqual(expectedOutput);
+		});
+	}
 });
--- util/tuple.ts
+++ util/tuple.ts
@@ -1,2 +1,5 @@
 export type Tuple<A, B> = [A, B];
 export type ReadonlyTuple<A, B> = readonly [A, B];
+
+export type Triple<A, B, C> = [A, B, C]
+export type ReadonlyTriple<A, B, C> = readonly [A, B, C];
