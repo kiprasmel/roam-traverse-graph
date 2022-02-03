471cf7c8dc28547a9f5a3e85aca88771da94fd05
Kipras Melnikovas <kipras@kipras.org>
2022-02-03 02:08:06 +0200

    prep for test
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- plugin-export-public-pages/parseASTFromBlockString.spec.ts
+++ plugin-export-public-pages/parseASTFromBlockString.spec.ts
@@ -1,3 +1,5 @@
-import { testcase } from "./parseASTFromBlockString.utils.spec";
+import { runMany } from "jest-sucks";
 
-//
+import { testcase, input1, expected1 } from "./parseASTFromBlockString.utils.spec";
+
+runMany([["should yeet", testcase(input1), expected1]]);
