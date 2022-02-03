50962f193f3c77453fafd1cf64c3773b18d93b00
Kipras Melnikovas <kipras@kipras.org>
2022-02-03 02:03:45 +0200

    TEMP
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- plugin-export-public-pages/parseASTFromBlockString.spec.ts
+++ plugin-export-public-pages/parseASTFromBlockString.spec.ts
@@ -1,5 +1,12 @@
-import { runMany } from "jest-sucks";
+// import { runMany } from "jest-sucks";
 
-import { testcase, input1, expected1 } from "./parseASTFromBlockString.utils.spec";
+import { testcase, input1, expected1 } from "./test-utils";
 
-runMany([["should yeet", testcase(input1), expected1]]);
+// runMany([["should yeet", testcase(input1), expected1]]);
+
+describe("block string AST parser", () => {
+	it("parses nested linked references", () => {
+		const result = testcase(input1);
+		expect(result).toStrictEqual(expected1);
+	});
+});
