e3a78eba905135cd8ffd34c39cb2d452dc1e18f0
Kipras Melnikovas <kipras@kipras.org>
2022-06-21 07:20:12 +0300

    implement `ASStoAST` - works correctly it seems
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- plugin-export-public-pages/blockStringToAST.ts
+++ plugin-export-public-pages/blockStringToAST.ts
@@ -1,3 +1,5 @@
+import assert from "assert"
+
 const LLBeginBoundaries = {
 	"::": {},
 	"#[[": {},
@@ -27,6 +29,10 @@ enum B {
 	"begin" = 1,
 	"end"   = 2,
 }
+noop(humanKind)
+function humanKind(b: B): "text" | "begin" | "end" {
+	return (["text", "begin", "end"] as const)[b]
+}
 
 // type TextNode = string
 type TextNode = readonly [B.text, string]
@@ -36,16 +42,86 @@ type StackNode = TextNode | BoundaryNode
 /** abstract syntax __stack__ */
 type ASS = StackNode[]
 
-type TreeNode = string | [Boundary, TreeNode]
+type TreeTextNode = string
+type TreeBoundaryNode = [Boundary, ...TreeNode[]]
+type TreeNode = TreeTextNode | TreeBoundaryNode
 /** abstract syntax __tree__ */
 type AST = TreeNode[]
 
 export function blockStringToASS(_str: string): ASS {
-	return [] // TODO
+	// return [] // TODO
+
+	return [ // TODO
+			[B.begin, "[["],
+				[B.text, "foo"],
+				[B.begin, "`"],
+					[B.text, "kek"],
+				[B.end, "`"],
+				[B.text, "w"],
+			[B.end, "[["],
+			[B.text, " bar baz"],
+		]
 }
 
-export function ASStoAST(_ass: ASS): AST {
-	return [] // TODO
+export function assertNever(x: never): never {
+	throw new Error(`expected x to be never, but got ${x}`)
+}
+
+noop(log)
+function log(...xs: any[]): void {
+	if (!!process.env.DEBUG) {
+		console.log(...xs)
+	}
+}
+
+export function ASStoAST(ass: ASS): AST {
+	return loop(0)[0]
+	//const r = loop(0)
+	//log(JSON.stringify(r, null, 2))
+	//return r[0]
+
+	function loop(initialPos: number): [ast: AST, processedCount: number] {
+		let ast: AST = []
+		let stack: Boundary[] = [] // when to return
+	
+		for (let pos = initialPos; pos < ass.length; pos++) {
+			const node: StackNode = ass[pos]
+			const [kind, value] = node
+
+			//log({ initialPos, pos, kind: humanKind(kind), value })
+	
+			if (kind === B.text) {
+				ast.push(value)
+			} else if (kind === B.begin) {
+				stack.push(value as Boundary) // TODO TS `is`
+				const [childAST, processedCount] = loop(pos + 1)
+
+				const tmp: TreeBoundaryNode = [value as Boundary, ...childAST] // TODO OPTIMIZE // TODO TS `is`
+				ast.push(tmp)
+
+				pos += processedCount - 1 // -1 because next loop cycle will increment
+			} else if (kind === B.end) {
+				if (stack.length) {
+					const last = stack.pop()
+					assert.deepStrictEqual(last, value)
+
+					/** can continue parsing */
+				} else {
+					/** cannot continue parsing anymore - our current stack is done */
+
+					const processedCount = pos - initialPos + 1 // + 1 because started at + 1
+					//log({ pos, initialPos, processedCount })
+					return [ast, processedCount]
+				}
+			} else {
+				assertNever(kind)
+			}
+		}
+
+		assert.deepStrictEqual(stack.length, 0)
+	
+		return [ast, ass.length - 1]
+	}
 }
 
 export function blockStringToAST(str: string): AST {
@@ -57,8 +133,6 @@ type TestRet = TestData[]
 
 function runTest([str, expTree, expStack]: TestData) {
 	console.log("run test:", str)
-
-	const assert = require("assert")
 
 	const outStack: ASS = blockStringToASS(str)
 	const outTree: AST = ASStoAST(outStack)
@@ -78,21 +152,33 @@ function runTests() {
 
 export const tests: TestRet = [
 	[
-		"[[foo]] bar baz",
+		"[[foo `kek` w]] bar baz",
 		[
 			["[[",
-				"foo"
+				"foo",
+				["`",
+					"kek"
+				],
+				"w",
 			],
 			" bar baz"
 		],
 		[
 			[B.begin, "[["],
 				[B.text, "foo"],
+				[B.begin, "`"],
+					[B.text, "kek"],
+				[B.end, "`"],
+				[B.text, "w"],
 			[B.end, "[["],
 			[B.text, " bar baz"],
 		],
 	],
 ]
+
+export function noop(..._xs: any[]): void {
+	// do nothing
+}
 
 if (!module.parent) {
 	runTests()
