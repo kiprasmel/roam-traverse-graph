e9cbcc77c3cdfded1491a6645bd8488844d6edb2
Kipras Melnikovas <kipras@kipras.org>
2022-06-24 20:58:56 +0300

    TEMP (multiple things, need to extract) works !! (doesn't respect privacy / need to switch config maybe)
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- plugin-export-public-pages/parseASTFromBlockString.ts
+++ plugin-export-public-pages/parseASTFromBlockString.ts
@@ -1,19 +1,35 @@
 import { MutatingActionToExecute } from "../traverseBlockRecursively";
 
 import { withMetadata } from "../util/withMetadata";
+import { ASS, ASStoAST, AST, blockStringToASS } from "./blockStringToAST";
 
 export const parseASTFromBlockString: MutatingActionToExecute<
 	{},
 	{
-		stack: Stack;
-		stackTree: StackTree;
+		ASS: ASS;
+		AST: AST;
 	}
 > = () => (block) => {
-	let a;
-	//
+	console.log("block.string", block.string);
+
+	// TODO REMOVE ME
+	const tmpBlacklist = [
+		"it gets called multiple times. i might as well stick to my", //
+		"ooh nvm, the sub-branches are still f'ed up,",
+		"://", // TODO HANDLE URLS
+		// "that's been out there in the last few days -- code blocks / ticks", // TODO SUPPORT EMPTY
+		"hook of [[git]] provides the name of the operation (rebase, amend)",
+		"hehe, classic mistake of `git pull` after rebase,",
+	];
+	const str: string = tmpBlacklist.some((x) => block.string.includes(x)) ? "" : block.string;
+
+	const ass: ASS = blockStringToASS(str);
+	const ast: AST = ASStoAST(ass);
+
+	console.log("ast", ast);
 
 	return withMetadata({
-		stack: stackWithTextInsteadOfChars,
-		stackTree,
+		ASS: ass,
+		AST: ast,
 	})(block);
 };
