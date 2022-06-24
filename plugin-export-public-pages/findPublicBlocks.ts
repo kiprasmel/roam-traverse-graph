87804dbbeb8c746215cc6ed840dbc960fb255d5c
Kipras Melnikovas <kipras@kipras.org>
2022-06-24 20:58:56 +0300

    TEMP (multiple things, need to extract) works !! (doesn't respect privacy / need to switch config maybe)
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- plugin-export-public-pages/findPublicBlocks.ts
+++ plugin-export-public-pages/findPublicBlocks.ts
@@ -3,6 +3,7 @@
 import { MutatingActionToExecute } from "../traverseBlockRecursively";
 import { PageWithMetadata } from "../types";
 import { withMetadata } from "../util/withMetadata";
+import { ASS, AST } from "./blockStringToAST";
 import { getLinkedReferences } from "./findLinkedReferencesOfABlock";
 
 export const removeUnknownProperties: MutatingActionToExecute<{}> = () => (block) =>
@@ -39,8 +40,8 @@ export const markBlockPublic: MutatingActionToExecute<
 	},
 	{
 		// hasCodeBlock: boolean;
-		stack: any;
-		stackTree: any;
+		ASS: ASS;
+		AST: AST;
 	}
 > = ({
 	// parentBlock,
@@ -62,7 +63,7 @@ export const markBlockPublic: MutatingActionToExecute<
 		// const hasSubstringNotInsideCode = (tags: string[]): boolean =>
 		// 	!block.metadata.hasCodeBlock && tags.some((tag) => block.string.includes(tag)); //
 
-		const linkedRefs: string[] = getLinkedReferences(block.metadata.stackTree);
+		const linkedRefs: string[] = getLinkedReferences(block.metadata.AST);
 
 		const hasPublicTag: boolean = publicTags.some((tag) => linkedRefs.includes(tag));
 		const hasPublicOnlyTag: boolean = publicOnlyTags.some((tag) => linkedRefs.includes(tag));
